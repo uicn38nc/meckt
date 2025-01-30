5eceda16d3b2c5ed8e6cf0c612329be4e646cfb0
Xorrad <monsieurs.aymeric@gmail.com>
2025-01-30 14:25:30 +0100

    fix: add temporarily a try/catch to print exceptions to crash.txt on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,25 @@
 #include "parser/Parser.hpp"
 
 int main() {
+#ifdef _WIN32
+    // Temporary use try/catch to print crashes to a
+    // file as backward is disabled on Windows.
+    try {
+        App app;
+        app.Init();
+        app.Run();
+        // Parser::Benchmark();
+    }
+    catch(std::exception& e) {
+        std::ofstream file(CRASH_FILE, std::ios::out);
+        file << e.what() << std::endl;
+        file.close();
+    }
+#else
     App app;
     app.Init();
     app.Run();
     // Parser::Benchmark();
+#endif
     return 0;
 }
